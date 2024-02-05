068dea01053b0234a11c56613540f413a1a10554
a7lasgod <151100807+a7lasgod@users.noreply.github.com>
2024-02-05 00:01:39 -0300

    Create area.java

---
+++ area.java
@@ -0,0 +1,14 @@
+import java.util.Locale;
+public class main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		double bma,bme,h, result;
+		
+		bma = 8;
+		bme = 6;
+		h = 5;
+		result = (bma + bme) / 2 * h;
+				
+				System.out.println("your result is: "+result);
+		
